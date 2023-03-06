0ed2fe3a385d51fde70b9533f6a3f67b5840c387
miiddb <101891716+miiddb@users.noreply.github.com>
2023-03-06 19:02:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+var cors = require('cors')
 
+app.use(cors())
 const server = require('http').Server(app);
 
 
