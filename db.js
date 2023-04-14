a6008b690c6bf29de8c389617cd2919719d4f71c
miiddb <101891716+miiddb@users.noreply.github.com>
2023-04-14 15:34:19 +0200

    Create db.js

---
+++ db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg')
+
+const pool = new Pool ({
+    user:process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD, 
+    host: process.env.DB_HOST,
+    database:process.env.DB_DATABASENAME,
+    port:5432,
+    ssl: true
+
+});
+
+module.exports = pool; //client;
